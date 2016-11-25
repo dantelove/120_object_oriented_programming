f28d2a36d57b80908e6427f7e50bfcdbf4fdf39f
dlove1 <outlineandconquer@gmail.com>
2016-11-25 18:44:44 +0000

    Lesson 5 - Tic Tac Toe Improvements

--- lesson_5/ttt2.rb
+++ lesson_5/ttt2.rb
@@ -98,13 +98,38 @@ end
 class TTTGame
   HUMAN_MARKER = "X"
   COMPUTER_MARKER = "O"
+  FIRST_TO_MOVE = HUMAN_MARKER
 
   attr_reader :board, :human, :computer
+  
+  def play
+    display_welcome_message
+    clear
+
+    loop do
+      display_board
+
+      loop do
+        current_player_moves
+        break if board.someone_won? || board.full?
+        clear_screen_and_display_board if human_turn?
+      end
+      display_result
+      break unless play_again?
+      reset
+      display_play_again_message
+    end
+
+    display_goodbye_message
+  end 
+  
+  private
 
   def initialize
     @board = Board.new
     @human = Player.new(HUMAN_MARKER)
     @computer = Player.new(COMPUTER_MARKER)
+    @current_marker = FIRST_TO_MOVE
   end
 
   def display_welcome_message
@@ -181,33 +206,23 @@ class TTTGame
 
   def reset
     board.reset
+    @current_marker = FIRST_TO_MOVE
     clear
   end
-
-  def play
-    display_welcome_message
-    clear
-
-    loop do
-      display_board
-
-      loop do
-        human_moves
-        break if board.someone_won? || board.full?
-
-        computer_moves
-        break if board.someone_won? || board.full?
-
-        clear_screen_and_display_board
-      end
-      display_result
-      break unless play_again?
-      reset
-      display_play_again_message
+  
+  def current_player_moves
+    if human_turn?
+      human_moves
+      @current_marker = COMPUTER_MARKER
+    else
+      computer_moves
+      @current_marker = HUMAN_MARKER
     end
-
-    display_goodbye_message
-  end 
+  end
+  
+  def human_turn?
+    @current_marker == HUMAN_MARKER
+  end
 end
 
 # we'll begin a new game like this
